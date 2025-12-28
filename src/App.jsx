440333d5482317041a5cb91bc80a61a44f834485
Guadalupe <gma.pedagoga@gmail.com>
2025-12-28 09:45:41 +0100

    plantilla base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+const [inputValue, setInputValue] = useState("");
+
+console.log(inputValue);
 
   return (
     <>
-
+<h1>Hola, ¿En qué puedo ayudarte?</h1>
+<form action="submit">
+  <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+<button>Enviar</button>
+</form>
+<div><p>Tu respuesta</p></div>
     </>
   )
 }
